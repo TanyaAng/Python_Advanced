637bf30c719f688467ecbaff1cc1cf9b72a06811
TanyaAng <t.j.angelova@gmail.com>
2022-05-17 23:11:36 +0300

    Exercises

--- 02_Lists_as_Stacks_and_Queues_Exercises/05_truck_tour.py
+++ 02_Lists_as_Stacks_and_Queues_Exercises/05_truck_tour.py
@@ -1,9 +1,28 @@
-petrol_stations=int(input())
+from collections import deque
 
-track_consumption=1
+number_stations=int(input())
+petrol_stations=deque([])
 petrols_in_tank=0
 
-for _ in range (petrol_stations):
-    p, d=input().split()
-    amount_petrol=int(p)
-    distance=int(d)
+for _ in range (number_stations):
+    petrol_stations.append(input())
+
+for big_station in range (number_stations):
+    is_valid = True
+    for small_station in range (number_stations):
+        current_amount,kilometers=petrol_stations[small_station].split()
+        current_amount=int(current_amount)
+        kilometers=int(kilometers)
+        petrols_in_tank += current_amount
+        if petrols_in_tank>=kilometers:
+            petrols_in_tank-=kilometers
+        else:
+            petrols_in_tank=0
+            is_valid=False
+            break
+    if is_valid:
+        print (big_station)
+        break
+    else:
+        petrol_stations.rotate(-1)
+
