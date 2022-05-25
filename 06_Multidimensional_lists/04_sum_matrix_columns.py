52c93aa24720698ec633bcc7008dd375305ed4e8
TanyaAng <t.j.angelova@gmail.com>
2022-05-25 20:46:07 +0300

    Lab Exercises

--- 06_Multidimensional_lists/04_sum_matrix_columns.py
+++ 06_Multidimensional_lists/04_sum_matrix_columns.py
@@ -1,7 +1,23 @@
+import sys
+from io import StringIO
+test_input1='''3, 6
+7 1 3 3 2 1
+1 3 9 8 5 6
+4 6 7 9 1 0
+
+'''
+
+test_input2='''3, 3
+1 2 3
+4 5 6
+7 8 9
+'''
+# sys.stdin=StringIO(test_input1)
+sys.stdin=StringIO(test_input2)
+
 def read_matrix():
     n,m=[int(x) for x in input().split(', ')]
     matrix=[]
-
     for i in range(n):
         row=[int(x) for x in input().split()]
         matrix.append(row)
