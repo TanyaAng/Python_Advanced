c9d61e93f3fb4216ff6ce51767910242ad5cb6a9
TanyaAng <t.j.angelova@gmail.com>
2022-06-05 11:32:32 +0300

    Lab Exercises

---
+++ 12_Error_Hadling_Exercises/01_numbers_dictionary.py
@@ -0,0 +1,40 @@
+def raise_exception_value_must_be_integer():
+    print(f"The variable number must be an integer")
+
+def raise_exception_number_does_not_exist():
+    print(f"Number does not exist in dictionary")
+
+numbers_dictionary = {}
+
+while True:
+    line = input()
+    if line == "Search":
+        break
+    try:
+        number_as_string = line
+        number = int(input())
+        numbers_dictionary[number_as_string] = number
+    except:
+        raise_exception_value_must_be_integer()
+
+while True:
+    line = input()
+    if line == "Remove":
+        break
+    searched = line
+    try:
+        print(numbers_dictionary[searched])
+    except:
+        raise_exception_number_does_not_exist()
+
+while True:
+    line = input()
+    if line == "End":
+        break
+    searched = line
+    try:
+        del numbers_dictionary[searched]
+    except:
+        raise_exception_number_does_not_exist()
+
+print(numbers_dictionary)
