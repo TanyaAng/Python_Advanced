8088e6b5d55cee64de1a6db8fa1e33956d41873e
TanyaAng <t.j.angelova@gmail.com>
2022-06-16 15:40:26 +0300

    Lab Exercises

---
+++ 10_Function_Advanced_Exercise/10_fill_the_box.py
@@ -0,0 +1,29 @@
+from collections import deque
+def fill_the_box(*args):
+    height=args[0]
+    length=args[1]
+    width=args[2]
+    cubes=args[3:]
+    volume=height*length*width
+    cubes_to_fill=deque([])
+    for c in cubes:
+        if c=='Finish':
+            break
+        else:
+            cubes_to_fill.append(c)
+    while cubes_to_fill:
+        c=cubes_to_fill.popleft()
+        if volume-c>=0:
+            volume-=c
+        else:
+            c-=volume
+            cubes_to_fill.appendleft(c)
+            return f"No more free space! You have {sum(cubes_to_fill)} more cubes."
+    return f"There is free space in the box. You could put {volume} more cubes."
+
+
+
+
+print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))
---
+++ 10_Function_Advanced_Exercise/11_math_operations.py
@@ -0,0 +1,29 @@
+def math_operations(*args, **kwargs):
+    numbers = args
+    dictionary=kwargs
+    round=0
+    for index in range (len(numbers)):
+            current_index=index-round*4
+            if current_index==0:
+                dictionary['a'] += numbers[index]
+            elif current_index== 1:
+                dictionary['s'] -= numbers[index]
+            elif current_index== 2 and not numbers[index] == 0:
+                dictionary['d'] /= numbers[index]
+            elif current_index== 3:
+                dictionary['m'] *= numbers[index]
+                round+=1
+    result=''
+    for key,value in sorted(dictionary.items(), key=lambda kvp: (-kvp[1], kvp[0])):
+        result+=f"{key}: {value:.1f}\n"
+    return result
+
+
+
+print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
+print(math_operations(-1.0, 0.5, 1.6, 0.5, 6.1, -2.8, 80.0, a=0, s=(-2.3), d=0, m=0))
+print(math_operations(6.0, a=0, s=0, d=5, m=0))
+
+
+
+
