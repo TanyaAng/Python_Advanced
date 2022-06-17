762f40672b7319817721e61c5abccf7f968267a7
TanyaAng <t.j.angelova@gmail.com>
2022-06-17 23:33:02 +0300

    Lab Exercises

--- 22_Exam_Preparation/03_naughty_or_nice.py
+++ 22_Exam_Preparation/03_naughty_or_nice.py
@@ -1,90 +1,105 @@
-def naughty_or_nice_list (*args,**kwargs):
-    kids_dict={'Nice':[],'Naughty':[], 'Not found':[]}
-    list_of_kids=args[0]
-    for i in range(1,len(args)):
-        number,type=args[i].split('-')
-        number=int(number)
-        kids_out_of_list = []
-        for kid in list_of_kids:
-            n,kid_name=kid
-            if number==n:
-                kids_dict[type].append(kid_name)
-                kids_out_of_list.append(kid)
-                break
-        if kids_out_of_list:
-            for kid in kids_out_of_list:
-                list_of_kids.remove(kid)
+def found_is_unique_by_number(kids, number):
+    is_unique = False
+    found_kid = None
+    for n, kid_name in kids:
+        if number == n and is_unique:
+            return False, None
+        if number == n:
+            is_unique = True
+            found_kid = kid_name
+    if is_unique:
+        return True, found_kid
+    else:
+        return False, None
+
+
+def found_is_unique_by_name(kids, name):
+    is_unique = False
+    number = None
+    found_kid = None
+    for n, kid_name in kids:
+        if name == kid_name and is_unique:
+            return False, None, None
+        if name == kid_name:
+            is_unique = True
+            found_kid = kid_name
+            number = n
+    if is_unique:
+        return True, number, found_kid
+    else:
+        return False, None, None
+
+
+def naughty_or_nice_list(kids, *args, **kwargs):
+    kids_dict = {'Nice': [], 'Naughty': [], 'Not found': []}
+    for command in args:
+        number, type = command.split('-')
+        number = int(number)
+        is_unique, found_kid = found_is_unique_by_number(kids, number)
+        if is_unique:
+            kids.remove((number, found_kid))
+            kids_dict[type].append(found_kid)
 
     if kwargs:
-        for name,type in kwargs.items():
-            kids_out_of_list=[]
-            for kid in list_of_kids:
-                n,kid_name=kid
-                if name==kid_name:
-                    kids_dict[type].append(kid_name)
-                    kids_out_of_list.append(kid)
-                    break
-            if kids_out_of_list:
-                for kid in kids_out_of_list:
-                    list_of_kids.remove(kid)
-    if list_of_kids:
-        for kid in list_of_kids:
+        for name, type in kwargs.items():
+            is_unique, found_number, found_kid = found_is_unique_by_name(kids, name)
+            if is_unique:
+                kids.remove((found_number, found_kid))
+                kids_dict[type].append(found_kid)
+
+    if kids:
+        for kid in kids:
             kids_dict['Not found'].append(kid[1])
 
-    result=''
-    for type,kids in kids_dict.items():
+    result = ''
+    for type, kids in kids_dict.items():
         if kids:
-            result+=f"{type}: {', '.join(kids)}\n"
-
+            result += f"{type}: {', '.join(kids)}\n"
     return result
 
 
-print(naughty_or_nice_list(
-    [
-        (3, "Amy"),
-        (1, "Tom"),
-        (7, "George"),
-        (3, "Katy"),
-    ],
-    "3-Nice",
-    "1-Naughty",
-    Amy="Nice",
-    Katy="Naughty",
-))
-
-
-print(naughty_or_nice_list(
-    [
-        (7, "Peter"),
-        (1, "Lilly"),
-        (2, "Peter"),
-        (12, "Peter"),
-        (3, "Simon"),
-    ],
-    "3-Nice",
-    "5-Naughty",
-    "2-Nice",
-    "1-Nice",
-    ))
-
-
-print(naughty_or_nice_list(
-    [
-        (6, "John"),
-        (4, "Karen"),
-        (2, "Tim"),
-        (1, "Merry"),
-        (6, "Frank"),
-    ],
-    "6-Nice",
-    "5-Naughty",
-    "4-Nice",
-    "3-Naughty",
-    "2-Nice",
-    "1-Naughty",
-    Frank="Nice",
-    Merry="Nice",
-    John="Naughty",
-))
-
-
+# print(naughty_or_nice_list(
+#     [
+#         (3, "Amy"),
+#         (1, "Tom"),
+#         (7, "George"),
+#         (3, "Katy"),
+#     ],
+#     "3-Nice",
+#     "1-Naughty",
+#     Amy="Nice",
+#     Katy="Naughty",
+# ))
+#
+# print(naughty_or_nice_list(
+#     [
+#         (7, "Peter"),
+#         (1, "Lilly"),
+#         (2, "Peter"),
+#         (12, "Peter"),
+#         (3, "Simon"),
+#     ],
+#     "3-Nice",
+#     "5-Naughty",
+#     "2-Nice",
+#     "1-Nice",
+# ))
+#
+# print(naughty_or_nice_list(
+#     [
+#         (6, "John"),
+#         (4, "Karen"),
+#         (2 , "Tim"),
+#         (1, "Merry"),
+#         (6, "Frank"),
+#     ],
+#     "6-Nice",
+#     "5-Naughty",
+#     "4-Nice",
+#     "3-Naughty",
+#     "2-Nice",
+#     "1-Naughty",
+#     Frank="Nice",
+#     Merry="Nice",
+#     John="Naughty",
+# ))
